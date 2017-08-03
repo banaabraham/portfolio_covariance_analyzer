785e3d5da3703bc03617afe18d4a3d2758f5fe24
Bana Abraham <bana.abraham@gmail.com>
2017-08-03 08:18:09 +0700

    Update portfolio_cov.py
    Support kompas 100 indonesian index stocks

--- portfolio_cov.py
+++ portfolio_cov.py
@@ -1,9 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Jul  2 19:52:29 2017
-
-@author: bana
-"""
 from matplotlib import pyplot as plt
 import statistics
 import pandas as pd
@@ -23,18 +17,18 @@ def average(s):
 #portfolio generator and return its covariance to a shared variable and choose which has the lowest covariance
 def analyze(dow,n,return_dict):
     count=0
-    a=10000
+    a=float('inf')
     for c in itertools.combinations(dow, n):
         count+=1
         if a>average(c):
             a=average(c)
             tick=c
-    return_dict[tick] =[a,count]        
+    return_dict[tick] = [a,count]        
 
 #dow 30 list stocks
 #dow="amzn aapl ba cat csco cvx dd dis fitb ge goog gs hd ibm intc jnj jpm ko mcd mmm mrk pfe pg trv tsla unh v vz wfc wmt xom yhoo"
 #dow=dow.split(" ")
-f = open("SP100.txt","r")
+f = open("kompas100.txt","r")
 dow = [x.strip("\n") for x in f.readlines()]
 stock_dict={}
 portfolio=[]
@@ -53,23 +47,47 @@ for ticker in dow:
         except:
             pass    
     data = d['Close'].values
-    #calculate monthly return
-    mth_return = []  
-    for i in range(len(data)-1,0,-30):
-        mth_return.append((data[i]-data[i-30])/data[i-30])
-    #calculating risk and covariance 
-    resiko = statistics.stdev(mth_return)
-    laba=(data[0]-data[-1])/data[-1]
-    if laba<0:
-        stock_dict[ticker] = 10000.0
+    if len(data)<200:
+        pass
     else:    
-        stock_dict[ticker] = float(resiko/(laba**2))
+    #calculate monthly return
+        mth_return = []  
+        for i in range(len(data)-1,0,-30):
+            mth_return.append((data[i]-data[i-30])/data[i-30])
+        #calculating risk and covariance 
+        resiko = statistics.stdev(mth_return)
+        laba=(data[0]-data[-1])/data[-1]
+        if laba<0:
+            stock_dict[ticker] = float('inf')
+        else: 
+            try:
+                stock_dict[ticker] = float(resiko)/float(laba**2)
+            except ZeroDivisionError :
+                break
+temp = [x for x in dow if x in stock_dict.keys() ]
+dow = temp                
+        
+def prop(s):
+    prop={}
+    bobot=[]
+    for i in s:
+        i = i+".csv"
+        d=pd.read_csv(i)
+        data = d['Close'].values
+        for i in range(len(data)-1,0,-30):
+            mth_return.append((data[i]-data[i-30])/data[i-30]) 
+        resiko = statistics.stdev(mth_return)
+        bobot.append(resiko)
+    bobotkuad = [x**2 for x in bobot]    
+    for i in range(len(bobot)):
+        prop[s[i]] = bobotkuad[i]/sum(bobotkuad)
+    return prop    
     
 if __name__ == '__main__':
     
     manager = Manager()
     return_dict = manager.dict()
-    n=list(range(3,5))
+    n=list(range(3,7))
     #calculating average covariance of the combinations of stocks using multi process
     thread=[]
     for i in n:
@@ -89,3 +107,4 @@ if __name__ == '__main__':
     plt.plot(a)
     print (sum(b))   
     print (return_dict)
+    print(prop(return_dict.keys()[0]))
